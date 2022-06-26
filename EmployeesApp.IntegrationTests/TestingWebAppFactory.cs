00eed456a6f085e3888aca0d54f95f8d348b871a
winsaludar <erwinsaludar@gmail.com>
2022-06-26 23:16:24 +0800

    Initial commit.

---
+++ EmployeesApp.IntegrationTests/TestingWebAppFactory.cs
@@ -0,0 +1,49 @@
+﻿using EmployeesApp.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EmployeesApp.IntegrationTests;
+
+public class TestingWebAppFactory<TEntryPoint> : WebApplicationFactory<Program> where TEntryPoint : Program
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureServices(services =>
+        {
+            var descriptor = services.SingleOrDefault(
+                d => d.ServiceType ==
+                    typeof(DbContextOptions<EmployeeContext>));
+
+            if (descriptor != null)
+                services.Remove(descriptor);
+
+            services.AddDbContext<EmployeeContext>(options =>
+            {
+                options.UseInMemoryDatabase("InMemoryEmployeeTest");
+            });
+
+            services.AddAntiforgery(t => 
+            {
+                t.Cookie.Name = AntiForgeryTokenExtractor.AntiForgeryCookieName;
+                t.FormFieldName = AntiForgeryTokenExtractor.AntiForgeryFieldName;
+            });
+
+            var sp = services.BuildServiceProvider();
+            using (var scope = sp.CreateScope())
+            using (var appContext = scope.ServiceProvider.GetRequiredService<EmployeeContext>())
+            {
+                try
+                {
+                    appContext.Database.EnsureCreated();
+                }
+                catch (Exception ex)
+                {
+                    //Log errors or do anything you think it's needed
+                    throw;
+                }
+            }
+        });
+    }
+}
